624b7921be14db21d1f1adc6ae43e7fa13f55c89
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-12 20:00:36 +0300

    fix: handle edge cases in prime number algorithm
    - Add check for numbers less than 2 (0, 1, and negatives).
    - Update loop condition to i <= number / 2 for better accuracy.
    - Refactor if-else logic to prevent logical errors.

--- projects/09_is_prime.c
+++ projects/09_is_prime.c
@@ -8,17 +8,18 @@ int main()
     int number,i;
     i=2;
 
-    printf("bir sayi giriniz:");
+    printf("enter a number:");
     scanf("%d",&number);
 
-    if(number<0){
-        printf("pozitif bir deger giriniz");
+    if(number < 2){
+        printf("%d is not a prime number.\n", number);
         return 0;
     }
  
-    while(i < number/2){
+    // Sayı 2 ise direkt asaldır (Döngüye girmeden aşağıya atlar)
+    while(i <= number/2){
         if(number%i == 0){
-            printf("%d bir asal sayi olamaz %d ile bolunebiliyor.",number,i);
+            printf("%d is not a prime number because it is divisible by %d.",number,i);
             return 0;
         }
         i++;
