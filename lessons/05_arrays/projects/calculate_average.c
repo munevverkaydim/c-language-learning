63a58bf4b82bf49f767b2ed50a7ed7a9c2d16730
Münevver Kaydım <kaydimmunevver@gmail.com>
2026-05-18 22:00:17 +0300

    feat: add average calculation using float array
    - Define a float array with a specific size to store numbers
    - Use scanf to take multiple float inputs from the user in one line
    - Calculate the average and print it with three decimal places (.3f)

---
+++ lessons/05_arrays/projects/calculate_average.c
@@ -0,0 +1,19 @@
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include<math.h>
+
+    /* üç sayının ortalamasını hesaplayan algoritma*/
+
+    int main()
+    { 
+        float number[3], average;
+        printf("enter 3 values:");
+        scanf("%f %f %f", &number[0], &number[1], &number[2]);
+
+        average = (number[0] + number[1] + number[2]) / 3;
+
+        printf(" average is %.3f", average);
+        
+
+        return 0;
+    }
